f1c7443e54fe80791f002a65eb2612811f8c45fb
reypryma <refanopradana@gmail.com>
2024-05-22 14:06:32 +0700

    Initial commit

---
+++ src/main/kotlin/com/rey/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.rey.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
